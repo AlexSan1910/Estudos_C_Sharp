ad5f7f2e78ea1bac9db89e505d24f7c03597b816
Alex Gomes Da Silva Filho <65171195+AlexSan1910@users.noreply.github.com>
2023-03-22 09:25:20 -0300

    Estrutura For, Exercicio sem orientação objeto

---
+++ Aula 34 - Estrutura repetitiva for/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System;
+
+namespace Course
+{
+    class Program{
+        static void Main(string[] args){
+            // devemos utilizar essa condição sempre quando identificarmos a quantidade de loop's o programa deve fazer,
+            // caso não sabemos a quantidade de repetições (lop's) devemos sempre utilizamos a confição while.
+
+            System.Console.Write("Quantos numeros inteiros você vai digitar? ");
+            int qtdNumberUser = int.Parse(Console.ReadLine(), provider: CultureInfo.InvariantCulture);
+
+
+            int soma = 0;
+            for(int i = 1; i <= qtdNumberUser; i++){ // basicamente eu preciso definir um numero de inicio, então eu criei uma variavel
+                                                     // na hora chamada "i" e defini o valor de numero 1, após isso eu defini uma condição
+                                                     // ou seja, essa condição é que ela deve repetir essa função enquanto o i for menor ou
+                                                     // igual ao numero que o usuario nos passou, e na ultima etapa, nós definimos que a 
+                                                     // variavel i receberá sempre um mais uma unidade até que se repita o tanto que foi 
+                                                     // predefinido.
+
+            Console.Write("Valor #{0}: ", i); // printando na tela do usuario o valor de numero 1, 2 e 3 conforme foi a quantidade que o usuario pediu
+            int valor = int.Parse(Console.ReadLine());// capturando a informação que o usuario vai digitar e adicionando na variavel valor
+            soma = soma + valor; // adicionando em forma de soma as variaveis que o usuario digitou, na variavel soma.
+            }
+
+            Console.WriteLine("Soma = " + soma);// ess print só será executado após a quantidade necessaria de repetições da função acima, 
+                                                // e nesse caso vai printar o valor somado de todos os numeros digitados pelo usuario.
+
+        }
+    }
+}
---
+++ Aula 36, 37, 38 - Calculo sem Orientação a Objeto/Program.cs
@@ -0,0 +1,40 @@
+﻿// exemplo de um programa feito sem orientação a objeto
+using System;
+using System.Globalization; // utilização do cultureInfo.InvarianteCulture para troca do ponto para virgula.
+
+namespace Course{
+    class Program{
+        static void Main(string[] args){
+
+            double xA, xB, xC; // lado A do triangulo X, lado B do triangulo X e o lado C do triangulo X.
+            double yA, yB, yC; // lado A do triangulo Y, lado B do triangulo Y e o lado C do triangulo Y.
+
+            Console.Write("entre com as medidas do triangulo X: "); // leitura da entrada dos dados dos 3 lados do triangulo X
+            xA = double.Parse(Console.ReadLine(), provider: CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), provider: CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), provider: CultureInfo.InvariantCulture);
+
+            Console.Write("entre com as medidas do triangulo Y: "); // leitura da entrada dos dados dos 3 lados do triangulo Y
+            yA = double.Parse(Console.ReadLine(), provider: CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), provider: CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), provider: CultureInfo.InvariantCulture);
+
+            //calculos Area trinagulo X
+            double pX = (xA + xB + xC) /  2.0;
+            double areaX = Math.Sqrt(pX * (pX - xA) * (pX - xB) * (pX - xC)); // calculo da area do triangulo x
+
+            //calculos Area trinagulo X
+            double pY = (yA + yB + yC) /  2.0;
+            double areaY = Math.Sqrt(pY * (pY - yA) * (pY - yB) * (pY - yC)); // calculo da area do triangulo y
+
+            Console.WriteLine("A area de X é igual a: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("A area de Y é igual a: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY){
+                Console.WriteLine("A area maior é do triangulo X");
+            }else{
+                Console.WriteLine("A area maior é do triangulo Y");
+            }
+        }
+    }
+}
