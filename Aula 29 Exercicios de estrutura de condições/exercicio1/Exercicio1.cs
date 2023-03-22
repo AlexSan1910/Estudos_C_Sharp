106357c9fd23ed46648ad3328fd781aa5814a5d8
Alex Gomes Da Silva Filho <65171195+AlexSan1910@users.noreply.github.com>
2023-03-22 09:35:00 -0300

    Exercicio 1 e 2

---
+++ Aula 29 Exercicios de estrutura de condições/exercicio1/Exercicio1.cs
@@ -0,0 +1,17 @@
+﻿// Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+
+namespace Course{
+    class Program{
+        static void Main(string[] args){
+
+            int _number = -1;
+
+            if(_number < 0){
+                Console.WriteLine("Esse numero é negativo");
+            }
+            else{
+                Console.WriteLine("Esse numero é positivo");
+            }
+        }
+    }
+}
---
+++ Aula 29 Exercicios de estrutura de condições/exercicio2/Exercicio2.cs
@@ -0,0 +1,20 @@
+﻿namespace Course{
+    class Program{
+        static void Main(string[] args){
+
+
+            // Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar
+
+            int number = 2;
+
+            if(number % 2 == 0){
+                Console.WriteLine("Esse numero é par");
+            }
+            else{
+                Console.WriteLine("Esse numero é impar");
+            }
+
+    
+        }
+    }
+}
